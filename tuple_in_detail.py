2ff6efb5e9eed3733c75f05f385ca0187368c1a9
VINAY KUMAR RAPAKA <107675897+Vinayrapaka1@users.noreply.github.com>
2022-07-12 17:49:28 +0530

    Add files via upload

---
+++ tuple_in_detail.py
@@ -0,0 +1,28 @@
+# a=(1)
+# print(type(a))
+
+# b=(1,2)
+# print(type(b))
+
+# a=('vinay','kazipet','warangal','hyderabad')
+# print(type(a))
+# a[2]='begumpet'
+
+# b='this is vinay'
+# b[0]='hello'
+# print(b)
+
+# c=('hi','hello','how')
+# print(c,type(c))
+# print(c[3])
+
+# b=(12,2.3,'hello',[1,2],('H','B'),None,True)
+# print(b,type(b))
+
+# a=(1,23,'hey',32.2,)
+# a[0]='vinay'
+
+# v=(2,3.4,'vizag',[2,7],('V','K'))
+# print(v.count(3.4))
+# print(v.index(2))
+
